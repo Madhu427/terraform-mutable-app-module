17a9587c91d7a19f0cd9f25cd517b79514dc99d8
Madhu427 <madhusudhanachary.k@gmail.com>
2022-03-19 22:10:59 +0530

    spaces

--- alb.tf
+++ alb.tf
@@ -4,13 +4,13 @@ resource "aws_lb_target_group" "tg" {
   protocol = "HTTP"
   vpc_id   = data.terraform_remote_state.vpc.outputs.VPC_ID
   health_check {
-    enabled = true
-    healthy_threshold = 2
+    enabled             = true
+    healthy_threshold   = 2
     unhealthy_threshold = 2
-    interval = 5
-    path = "/health"
-    port = var.APP_PORT
-    timeout = 3
+    interval            = 5
+    path                = "/health"
+    port                = var.APP_PORT
+    timeout             = 3
   }
 }
 
