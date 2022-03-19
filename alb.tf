0691efbdf110f1b8b8de03dd347f562a5b06ea91
Madhu427 <madhusudhanachary.k@gmail.com>
2022-03-19 22:55:29 +0530

    "PRIVATE_HOST_ZONE_ID" & r53 added

--- alb.tf
+++ alb.tf
@@ -5,9 +5,9 @@ resource "aws_lb_target_group" "tg" {
   vpc_id   = data.terraform_remote_state.vpc.outputs.VPC_ID
   health_check {
     enabled             = true
-    healthy_threshold   = 10
-    unhealthy_threshold = 10
-    interval            = 30
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    interval            = 5
     path                = "/health"
     port                = var.APP_PORT
     timeout             = 10
